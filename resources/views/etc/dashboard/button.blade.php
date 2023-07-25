ad722f7f3095030131be748a386010de3eb92968
jakues <jakueenak@gmail.com>
2023-07-25 20:17:08 +0700

    Separate button file

---
+++ resources/views/etc/dashboard/button.blade.php
@@ -0,0 +1,4 @@
+<a href='javascript:void(0)' data-id="{{ $data->id }}" class="btn btn-sm btn-warning mr-5 edit-btn"><i
+        class="fa-regular fa-pen-to-square"></i></a>
+<a href='javascript:void(0)' data-id="{{ $data->id }}" class="btn btn-sm btn-error delete-btn"><i
+        class="fa-solid fa-eraser"></i></a>
