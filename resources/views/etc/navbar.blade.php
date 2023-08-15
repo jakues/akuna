af23fc435c06d60ee37f16d6c8522ce1add85639
jakues <jakueenak@gmail.com>
2023-08-15 19:01:05 +0700

    Minor fix for navbar

--- resources/views/etc/navbar.blade.php
+++ resources/views/etc/navbar.blade.php
@@ -32,7 +32,7 @@
                         @auth
                             @if (auth()->user()->role == 'admin')
                                 <li>
-                                    <a href="/manage"
+                                    <a href="{{ route('admin.index') }}"
                                         class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
                                         <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900"
                                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
