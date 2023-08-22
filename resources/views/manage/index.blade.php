fe8e0f9ed0619aabef00c1aad6b7aa36c7a5a518
jakues <jakueenak@gmail.com>
2023-08-22 14:16:24 +0700

    Fix css

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ asset('build/assets/app-c4a42d02.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-9726d929.css') }}">
     <title>manage - dashboard</title>
 </head>
 
